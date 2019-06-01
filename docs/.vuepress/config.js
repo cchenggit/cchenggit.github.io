0ccff7efc6c22885c6cd4a5335edec33e1620e89
cchenggit <654031023@qq.com>
2019-06-02 01:12:08 +0800

    blog source code

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,69 @@
+module.exports = {
+  title: "cchenggit 的小站",
+  description: '用文字记录思想.',
+  dest: 'public',
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
+    ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
+  ],
+  theme: 'reco',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/', icon: 'reco-home' },
+      { text: '时间轴', link: '/timeLine/', icon: 'reco-date' },
+      { text: '联系方式', 
+        icon: 'reco-message',
+        items: [
+          { text: 'GitHub', link: 'https://github.com/cchenggit', icon: 'reco-github' }
+        ]
+      }
+    ],
+    // 博客设置
+    blogConfig: {
+      category: {
+        location: 2, // 在导航栏菜单中所占的位置，默认2
+        text: '分类' // 默认 “分类”
+      },
+      tag: {
+        location: 3, // 在导航栏菜单中所占的位置，默认3
+        text: '标签' // 默认 “标签”
+      }
+    },
+    logo: '/head.png',
+    // 搜索设置
+    search: true,
+    searchMaxSuggestions: 10,
+    // 自动形成侧边导航
+    sidebar: 'auto',
+    // 最后更新时间
+    lastUpdated: 'Last Updated',
+    // 作者
+    author: 'cchenggit',
+    // 备案号
+    record: '无',
+    // 项目开始时间
+    startYear: '2019'
+    /**
+     * 密钥 (if your blog is private)
+     */
+
+    // keyPage: {
+    //   keys: ['your password'],
+    //   color: '#42b983',
+    //   lineColor: '#42b983'
+    // },
+
+    /**
+     * valine 设置 (if you need valine comment )
+     */
+
+    // valineConfig: {
+    //   appId: '...',// your appId
+    //   appKey: '...', // your appKey
+    // }
+  },
+  markdown: {
+    lineNumbers: true
+  },
+  plugins: ['@vuepress/medium-zoom', 'flowchart']
+}  
---
+++ docs/README.md
@@ -0,0 +1,22 @@
+---
+home: true
+heroImage: /gakki.jpg
+heroImageStyle: {
+  maxWidth: '600px',
+  width: '100%',
+  display: block,
+  margin: '9rem auto 2rem',
+  background: '#fff',
+  borderRadius: '1rem',
+}
+isShowTitleInHome: false
+actionText: 进入
+actionLink: /views/other/guide
+features:
+- title: 技术
+  details: 记录个人技术学习和总结
+- title: 生活
+  details: 总有些磕磕绊绊的事情,总有些欣喜开心的事情
+- title: Tomorrow
+  details: 生命不止, 精神不灭
+---
